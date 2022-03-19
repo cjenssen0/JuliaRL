a8ea9feef2806f6cc06519ceb7ff4c43caa38a30
chje <chje@sentient.no>
2022-03-19 16:41:03 +0100

    Misc cleanup for `state_mapping`

--- src/Transformations.jl
+++ src/Transformations.jl
@@ -11,10 +11,10 @@ Mapping from continuous state-space to discrete for cartpole environment.
 For now, hardcoded bins...
 """
 function state_mapping(s)
-    s_bin1 = [-3,-1.5,0,1.5,3]
-    s_bin2 = [-100,-10,0,10,100]
-    s_bin3 = [-0.3,-0.15,0.,0.15,0.3]
-    s_bin4 = [-100,-10,0,10,100]
+    s_bin1 = range(-3, 3, length=nbins)
+    s_bin2 = range(-100, 100, length=nbins)
+    s_bin3 = range(-0.3, 0.3, length=nbins)
+    s_bin4 = range(-100, 100, length=nbins)
     s_bins = [s_bin1, s_bin2, s_bin3, s_bin4]
     obs = [findnearest(s_bin, s[i]) for (i,s_bin) in enumerate(s_bins)]
     return obs
